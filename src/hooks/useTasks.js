633b8f154bc8c34c712f5fa251f91ca23ddc3a13
Diego Giraldo <diegowst@gmail.com>
2021-09-30 10:32:39 -0500

    add dynamic api tasks

--- src/hooks/useTasks.js
+++ src/hooks/useTasks.js
@@ -1,9 +1,17 @@
-import { createContext, useContext, useReducer } from 'react';
+import { createContext, useContext, useEffect, useReducer } from 'react';
+import axios from 'axios';
 
 const initialState = [];
 
+const repoState = {
+  open: 'todo',
+  closed: 'done'
+}
+
 function taskReducer(state, action) {
   switch (action.type) {
+    case 'batch_add':
+      return [...state, ...action.payload.tasks];
     case 'add':
       return [...state, action.payload.task];
     case 'update':
@@ -24,6 +32,28 @@ function useTasksProvider() {
   const reduce = (task, type) => dispatch({ type, payload: { task }});
   const add = task => reduce(task, 'add');
   const update = task => reduce(task, 'update');
+
+  useEffect(() => {
+    let tasks = [];
+
+    const getTasks = async () => {
+      tasks = await axios.get('https://api.github.com/repos/every-io/demo-issues/issues?state=all');
+      dispatch({
+        type: 'batch_add',
+        payload: {
+          tasks: tasks.data.map(repo => ({
+            id: repo.id,
+            label: repo.title,
+            status: repoState[repo.state]
+          }))
+        }
+      })
+    }
+
+    getTasks();
+
+
+  }, []);
 
   return {
     add,
